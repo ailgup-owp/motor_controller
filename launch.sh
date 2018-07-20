f2bad5bb9b1120b1005d851af2a01733e4157eb9
Your Name <chris.puglia@openwaterpower.com>
2018-07-20 13:04:55 -0400

    pi

--- launch.sh
+++ launch.sh
@@ -1,15 +1,8 @@
-
 while ! ping -c 1 -W 1 8.8.8.8; do
-
     echo "Waiting ..."
-
     sleep 1
-
     done
 git -C /home/pi/motor_tester fetch --all
-
 git -C /home/pi/motor_tester reset --hard origin/master
-
 sudo chmod +x /home/pi/motor_tester/launch.sh
-
 python3 /home/pi/motor_tester/motor_test.py &
