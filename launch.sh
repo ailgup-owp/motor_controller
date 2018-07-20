a13b4b323d8cb50bf9995c0ed76ba73aaf01b70b
Your Name <chris.puglia@openwaterpower.com>
2018-07-20 13:43:47 -0400

    pi

--- launch.sh
+++ launch.sh
@@ -1,7 +1,14 @@
+COUNT=1
 while ! ping -c 1 -W 1 8.8.8.8; do
+    if [ "$COUNT" -gt 5 ]
+    then
+      break
+    fi
     echo "Waiting ..."
     sleep 1
+    COUNT=$(($COUNT+1))
     done
 git -C /home/pi/motor_tester fetch --all
 git -C /home/pi/motor_tester reset --hard origin/master
 python3 /home/pi/motor_tester/motor_test.py &
+
