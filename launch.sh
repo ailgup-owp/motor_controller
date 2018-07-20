078fae74d1fd4d801b41fcc65db94d91d094428f
Chris <ailgup@mail.com>
2018-07-20 12:49:58 -0400

    stu

--- launch.sh
+++ launch.sh
@@ -11,6 +11,6 @@ git fetch --all
 
 git reset --hard origin/master
 
-sudo chmod +x /home/pi/motor_tester/launch.sh
+chmod +x /home/pi/motor_tester/launch.sh
 
 python3 /home/pi/motor_tester/motor_test.py &
