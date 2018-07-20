63ef76f701be8069cc5512b6a4217fa60803d89a
Chris <ailgup@mail.com>
2018-07-20 12:44:55 -0400

    stu

--- launch.sh
+++ launch.sh
@@ -1,6 +1,16 @@
 cd /home/pi/motor_tester
-sleep 5
+
+while ! ping -c 1 -W 1 8.8.8.8; do
+
+    echo "Waiting ..."
+
+    sleep 1
+
+    done
 git fetch --all
+
 git reset --hard origin/master
+
 sudo chmod +x /home/pi/motor_tester/launch.sh
+
 python3 /home/pi/motor_tester/motor_test.py &
