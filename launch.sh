28a9dde1e7d7249f98cdbb23a0ed139cab5a5654
Your Name <chris.puglia@openwaterpower.com>
2018-07-20 13:08:43 -0400

    hi

--- launch.sh
+++ launch.sh
@@ -4,5 +4,4 @@ while ! ping -c 1 -W 1 8.8.8.8; do
     done
 git -C /home/pi/motor_tester fetch --all
 git -C /home/pi/motor_tester reset --hard origin/master
-sudo chmod +x /home/pi/motor_tester/launch.sh
 python3 /home/pi/motor_tester/motor_test.py &
